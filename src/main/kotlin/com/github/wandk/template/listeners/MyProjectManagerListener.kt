d4a53a768dcc86fbeb16f053284da0c4e596cb8b
GitHub Action <action@github.com>
2021-10-20 13:40:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/wandk/template/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wandk/template/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wandk.template.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wandk.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/test/kotlin/com/github/wandk/template/MyPluginTest.kt
+++ src/test/kotlin/com/github/wandk/template/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wandk.template
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
